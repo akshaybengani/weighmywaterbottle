0dff77e3136bd96dc8ac2f72ec0beceb8f7bd73c
Akshay Bengani <akshay@cashee.com>
2022-01-18 19:41:28 +0530

    Added and tested health sdk checkout health_service in services tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: avoid_print, constant_identifier_names, use_key_in_widget_constructors
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:weighmywaterbottle/services/health_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +31,18 @@ class WeighMyWaterBottle extends StatelessWidget {
           alignment: Alignment.center,
           child: ElevatedButton(
             child: const Text("Test"),
-            onPressed: () async {},
+            onPressed: () async {
+              bool permissionStatus =
+                  await HealthService.instance.requestPermission;
+
+              if (permissionStatus) {
+                bool recordStatus =
+                    await HealthService.instance.logWater(waterML: 100);
+                print("Water record status $recordStatus");
+              } else {
+                print("Water Logging permission denied");
+              }
+            },
           ),
         ),
       ),
---
+++ lib/services/health_service.dart
@@ -0,0 +1,50 @@
+import 'package:health/health.dart';
+
+class HealthService {
+  static final HealthService instance = HealthService._();
+  HealthService._();
+
+  final List<HealthDataType> _type = [HealthDataType.WATER];
+  final List<HealthDataAccess> _access = [HealthDataAccess.WRITE];
+  final List<HealthDataAccess> _permission = [HealthDataAccess.READ_WRITE];
+
+  HealthFactory _health = HealthFactory();
+
+  Future<bool> get requestPermission async {
+    return _health.requestAuthorization(_type, permissions: _permission);
+  }
+
+  Future<bool> logWater({
+    required double waterML,
+  }) async {
+    final now = DateTime.now();
+    final earlier = now.subtract(const Duration(seconds: 5));
+
+    // 1ml = 0.001
+    // 10ml = 0.01
+    // 100ml = 0.1
+    // 1000ml = 1
+    // divide by 1000
+
+    final double intake = waterML / 1000;
+
+    bool? hasPermissions = await HealthFactory.hasPermissions(
+      _type,
+      permissions: _access,
+    );
+
+    if (hasPermissions == false) {
+      await _health.requestAuthorization(_type, permissions: _permission);
+    }
+
+    bool success = await _health.writeHealthData(
+      intake,
+      HealthDataType.WATER,
+      earlier,
+      now,
+    );
+
+    print("Water intake is $success");
+    return success;
+  }
+}
