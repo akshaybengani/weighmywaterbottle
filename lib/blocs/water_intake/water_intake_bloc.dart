a2920a07bd6d8042d189490d33319b16ec05ea78
Akshay Bengani <akshay@cashee.com>
2022-01-22 13:58:15 +0530

    Designed water update screen

--- lib/blocs/water_intake/water_intake_bloc.dart
+++ lib/blocs/water_intake/water_intake_bloc.dart
@@ -4,28 +4,71 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 abstract class WaterIntakeEvent {}
 
 class UpdateWeight extends WaterIntakeEvent {
-  final double weight;
+  final int weight;
 
   UpdateWeight({
     required this.weight,
   });
 }
 
+class LoadLastBottleWeight extends WaterIntakeEvent {}
+
+class RecordWeight extends WaterIntakeEvent {}
+
 // State
 abstract class WaterIntakeState {}
 
 class WaterIntakeLoading extends WaterIntakeState {}
 
-class WaterIntakeLoaded extends WaterIntakeState {}
+class TextUpdate extends WaterIntakeState {
+  final String text;
+
+  TextUpdate(final this.text);
+}
 
 // Bloc
 class WaterIntakeBloc extends Bloc<WaterIntakeEvent, WaterIntakeState> {
-  WaterIntakeBloc() : super(WaterIntakeLoaded());
+  WaterIntakeBloc() : super(WaterIntakeLoading());
+
+  int lastBottleWeight = 0;
+  int emptyBottleWeight = 0;
+  int fullBottleWeight = 0;
+  int currentBottleWeight = 0;
 
   @override
   Stream<WaterIntakeState> mapEventToState(
     final WaterIntakeEvent event,
   ) async* {
-    if (event is UpdateWeight) {}
+    if (event is LoadLastBottleWeight) {
+      yield WaterIntakeLoading();
+
+      await Future.delayed(const Duration(seconds: 1));
+      lastBottleWeight = 1000;
+      emptyBottleWeight = 500;
+      fullBottleWeight = 1500;
+      currentBottleWeight = lastBottleWeight;
+
+      yield TextUpdate("");
+    } else if (event is UpdateWeight) {
+      // print(event.weight);
+
+      currentBottleWeight = event.weight;
+
+      String text = "";
+      if (lastBottleWeight >= currentBottleWeight) {
+        text =
+            "Your last bottle weight is $lastBottleWeight\nYour current water weight is $currentBottleWeight\n\n $lastBottleWeight - $currentBottleWeight = ${lastBottleWeight - currentBottleWeight} grams";
+      } else {
+        text =
+            "Your last bottle weight is $lastBottleWeight\nYour current water weight is $currentBottleWeight\n\n Your current weight is higher then the last one, it means you have refilled the water bottle and drinked all of it ";
+      }
+
+      yield TextUpdate(text);
+    } else if (event is RecordWeight) {
+      yield WaterIntakeLoading();
+
+      await Future.delayed(const Duration(seconds: 1));
+      yield TextUpdate("");
+    }
   }
 }
--- lib/screens/water_intake/water_intake_screen.dart
+++ lib/screens/water_intake/water_intake_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weighmywaterbottle/blocs/water_intake/water_intake_bloc.dart';
+import 'package:weighmywaterbottle/services/wayfinder.dart';
 
 class WaterIntakeScreen extends StatefulWidget {
   const WaterIntakeScreen({Key? key}) : super(key: key);
@@ -8,21 +11,182 @@ class WaterIntakeScreen extends StatefulWidget {
 }
 
 class _WaterIntakeScreenState extends State<WaterIntakeScreen> {
+  late WaterIntakeBloc _waterIntakeBloc;
+
+  static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   TextStyle textStyle = const TextStyle(
     fontSize: 16,
   );
 
   @override
+  void initState() {
+    super.initState();
+    _waterIntakeBloc = WaterIntakeBloc();
+    _waterIntakeBloc.add(LoadLastBottleWeight());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            "Add Bottle Weight",
-            style: textStyle,
-          )
-        ],
+    return BlocProvider<WaterIntakeBloc>(
+      create: (final BuildContext _) {
+        return _waterIntakeBloc;
+      },
+      child: BlocBuilder<WaterIntakeBloc, WaterIntakeState>(
+        builder: (final BuildContext context, final WaterIntakeState state) {
+          return _body(state);
+        },
       ),
     );
+  }
+
+  Widget _body(WaterIntakeState state) {
+    if (state is WaterIntakeLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else if (state is TextUpdate) {
+      return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Wayfinder.instance.pop();
+            },
+          ),
+          title: const Text(
+            "Update Bottle Weight",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 20,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Form(
+          key: _formKey,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 18),
+            child: ListView(
+              children: [
+                const SizedBox(height: 18),
+                Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.black),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    "Your Full Bottle Weight is ${_waterIntakeBloc.fullBottleWeight}\nYour Empty Bottle Weight is ${_waterIntakeBloc.emptyBottleWeight}",
+                    style: const TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 18),
+                TextFormField(
+                  autofocus: true,
+                  initialValue: _waterIntakeBloc.currentBottleWeight.toString(),
+                  validator: (final String? value) {
+                    if (value == null) {
+                      return "Field can't be empty, please enter some value";
+                    } else if (value.isEmpty) {
+                      return "Field can't be empty, please enter some value";
+                    } else if (int.tryParse(value) == null) {
+                      return "Only numeric value is accepted";
+                    } else if (int.parse(value) <
+                        _waterIntakeBloc.emptyBottleWeight) {
+                      return "Bottle weight cannot be lower then empty bottle weight";
+                    } else if (int.parse(value) >
+                        _waterIntakeBloc.fullBottleWeight) {
+                      return "Bottle weight cannot be higher then full bottle weight";
+                    }
+                  },
+                  onChanged: (final String value) {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      _waterIntakeBloc.add(
+                        UpdateWeight(weight: int.parse(value)),
+                      );
+                    }
+                  },
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  cursorColor: Colors.black,
+                  decoration: const InputDecoration(
+                    labelText: "Current Bottle Weight",
+                    labelStyle: TextStyle(fontSize: 24, color: Colors.black),
+                    errorStyle: TextStyle(color: Colors.red),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(18)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(18)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(18)),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(18)),
+                    ),
+                  ),
+                ),
+                if (state.text.isNotEmpty) const SizedBox(height: 18),
+                if (state.text.isNotEmpty)
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.black),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      state.text,
+                      style: const TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                const SizedBox(height: 18),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Text(
+                      "RECORD ${_waterIntakeBloc.lastBottleWeight - _waterIntakeBloc.currentBottleWeight}",
+                      style: const TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
   }
 }
